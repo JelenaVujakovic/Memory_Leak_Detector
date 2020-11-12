38f820c732eab84c554fc6b701890e95b4e9d9f8
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2020-11-12 10:56:07 +0100

    Add files via upload

---
+++ application.cpp
@@ -0,0 +1,32 @@
+#include "memory_leak_detector.h"
+#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//Application structures
+typedef struct emp_ {
+    char emp_name[30];
+    unsigned int emp_id;
+    unsigned int age;
+    struct emp* mgr;
+    float salary;
+    int* p;
+}emp_t;
+
+typedef struct student_ {
+    char stud_name[32];
+    unsigned int rollno;
+    unsigned int age;
+    float aggregate;
+    struct student_* best_colleage;
+}student_t;
+
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
+
+
