5483ea39abe89d2997825b10d799d903275a3bc7
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2020-11-13 15:11:59 +0100

    Header file contains APIs for MLD algorithm

--- memory_leak_detector_algorithm.h
+++ memory_leak_detector_algorithm.h
@@ -2,16 +2,12 @@
 #define MEMORY_LEAK_DETECTOR_ALGORITHM_H_INCLUDED
 
 
-/*APIs to register root objects*/
-void mld_register_root_object (object_db_t *object_db,
-                               void *objptr,
-                               char *struct_name,
-                               unsigned int units);
+//APIs to register root objects
+void mld_register_root_object (object_db_t *object_db, void *objptr, char *struct_name, unsigned int units);
 
 void set_mld_object_as_global_root(object_db_t *object_db, void *obj_ptr);
 
-
-/*APIs for MLD Algorithm*/
+//APIs for Memory Leak Detector Algorithm
 void run_mld_algorithm(object_db_t *object_db);
 
 void report_leaked_objects(object_db_t *object_db);
